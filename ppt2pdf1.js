09b5e6fc59fa830e8c1c55d737febb12c42c3e9c
姚嘉松 <yjson@changxindeMacBook-Pro.local>
2021-05-09 10:53:03 +0800

    feat: first commmit

---
+++ ppt2pdf1.js
@@ -0,0 +1,72 @@
+const { PDFNet } = require('@pdftron/pdfnet-node');
+
+
+const runOfficeToPDF = async () => {
+    const inputPath = './TestFiles/';
+    const outputPath = inputPath + 'Output/';
+
+    const simpleDocxConvert = async (inputFilename, outputFilename) => {
+        const pdfdoc = await PDFNet.Convert.officeToPdfWithPath(inputPath + inputFilename);
+        await pdfdoc.save(outputPath + outputFilename, PDFNet.SDFDoc.SaveOptions.e_linearized);
+        console.log('Saved ' + outputFilename);
+    }
+
+    const pptToSvg = async (inputFilename, outputFilename) => {
+        const pdfdoc = await PDFNet.Convert.officeToPdfWithPath(inputPath + inputFilename);
+        await PDFNet.Convert.docToSvg(pdfdoc, outputPath + outputFilename)
+    }
+
+    const flexibleDocxConvert = async (inputFilename, outputFilename) => {
+        const pdfdoc = await PDFNet.PDFDoc.create();
+        pdfdoc.initSecurityHandler();
+        const options = new PDFNet.Convert.OfficeToPDFOptions();
+        options.setSmartSubstitutionPluginPath(inputPath);
+        const conversion = await PDFNet.Convert.streamingPdfConversionWithPdfAndPath(pdfdoc, inputPath + inputFilename, options);
+
+        while (await conversion.getConversionStatus() === PDFNet.DocumentConversion.Result.e_Incomplete) {
+            await conversion.convertNextPage();
+        }
+
+        if (await conversion.getConversionStatus() === PDFNet.DocumentConversion.Result.e_Success) {
+            const num_warnings = await conversion.getNumWarnings();
+            for (let i = 0; i < num_warnings; ++i) {
+                console.log('Conversion Warning: ' + await conversion.getWarningString(i));
+            }
+
+            // save the result
+            await pdfdoc.save(outputPath + outputFilename, PDFNet.SDFDoc.SaveOptions.e_linearized);
+            // done
+            console.log('Saved ' + outputFilename);
+        } else {
+            console.log('Encountered an error during conversion: '
+                + await conversion.getErrorString());
+        }
+    }
+
+    const main = async () => {
+        PDFNet.addResourceSearchPath('./Resources');
+
+        try {
+            // await simpleDocxConvert('mysql.pptx', 'mysql.pdf');
+            await pptToSvg('mysql.pptx', '/svg/mysql.svg');
+            await pptToSvg('p1.pptx', '/svg1/mysql.svg');
+            await pptToSvg('p2.pptx', '/svg2/mysql.svg');
+            // await flexibleDocxConvert('mysql.pptx',
+            //     'mysql1.pdf');
+
+            // await flexibleDocxConvert('factsheet_Arabic.docx', 'factsheet_Arabic.pdf');
+        } catch (err) {
+            console.log(err);
+        }
+
+        console.log('Done.');
+    }
+
+    PDFNet.runWithCleanup(main).catch(function (error) {
+        console.log('Error: ' + JSON.stringify(error));
+    }).then(function () { PDFNet.shutdown(); });
+}
+
+
+
+runOfficeToPDF();
